854f7f6b9fc598dfbec43ab17e69a006c8a25f09
mintsj1 <mintsj1@student.op.ac.nz>
2025-10-08 16:01:13 +1300

    working on comment posts

--- 1throwaway/socialgg/src/routes/comment-section/+page.svelte
+++ 1throwaway/socialgg/src/routes/comment-section/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-
+import Comment from '$lib/components/comment';
+let { data } = $props();
 </script>
 
-<h2>Comments</h2>
-
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores labore quae maxime sunt! Blanditiis deserunt obcaecati perspiciatis vitae ipsam repellat ad dignissimos cumque aliquid veniam neque hic, iste fugit quas!</p>
+{#each data.comments as comment}
+    <Comment data={comment} />
+{/each}

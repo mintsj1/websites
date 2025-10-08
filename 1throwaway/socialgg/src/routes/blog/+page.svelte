c5cfc6d372fd36328a3680a4cc27b9867e53de3f
mintsj1 <mintsj1@student.op.ac.nz>
2025-10-08 16:00:22 +1300

    added data prop to iterate blog posts

--- 1throwaway/socialgg/src/routes/blog/+page.svelte
+++ 1throwaway/socialgg/src/routes/blog/+page.svelte
@@ -1,10 +1,13 @@
+<script>
+    let { data } = $props();
+</script>
+
 <h2>Bloggers Contents</h2>
 
 <nav>
-    <a href="/blog/what">What</a>
-    <a href="/blog/current-affairs">Current Affairs</a>
-    <a href="/blog/who">Who</a>
-    <a href="blog-writing">Blog Writing</a>
+    {#each data.post as post}
+    <a href="/blog/{post.title}">{post.title}</a>
+    {/each}
 </nav>
 
 <style>

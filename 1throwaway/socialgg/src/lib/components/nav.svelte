1d920a2244f1684acd5f541f023b004c879af82e
mintsj1 <mintsj1@student.op.ac.nz>
2025-10-08 15:35:10 +1300

    Build the nav bar

---
+++ 1throwaway/socialgg/src/lib/components/nav.svelte
@@ -0,0 +1,27 @@
+<script>
+    // Navigation component
+
+</script>
+
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About us</a></li>
+        <li><a href="/comment-section">Comments</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        border: 1px solid black;
+        background-color: #ccc;
+    }
+    ul {
+        list-style-type: none;
+        display: flex;
+    }
+
+    li {
+        padding: 1em;
+    }
+</style>

d97f9f36af7113b9fbea537fdcae3bb730cfca11
mintsj1 <mintsj1@student.op.ac.nz>
2025-10-08 15:36:30 +1300

    added header element and p>lorem

---
+++ 1throwaway/socialgg/src/routes/comment-section/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+
+</script>
+
+<h2>Comments</h2>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores labore quae maxime sunt! Blanditiis deserunt obcaecati perspiciatis vitae ipsam repellat ad dignissimos cumque aliquid veniam neque hic, iste fugit quas!</p>
